aab27597312250d44863783a20313a90513bb0e8
kramlat (Mark Toman) <kram2048@gmail.com>
2026-04-02 03:49:55 -0600

    type safe unions are ready to use. cleaned up source

--- src/Types.h
+++ src/Types.h
@@ -3,10 +3,36 @@
 #include <stdint.h>
 
 //Fixed point ints
-typedef union
-{
-	struct
-	{
+typedef union {
+	struct {
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+	} f;
+	uint8_t v;
+} byte_u;
+
+typedef union {
+	struct {
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+	} f;
+	int8_t v;
+} byte_s;
+
+typedef union {
+	struct {
 		#ifdef SCP_BIG_ENDIAN
 			int8_t u;
 			uint8_t l;
@@ -15,6 +41,43 @@ typedef union
 			int8_t u;
 		#endif
 	} f;
+	struct {
+		#ifdef SCP_BIG_ENDIAN
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+		unsigned b8 : 1;
+		unsigned b9 : 1;
+		unsigned b10 : 1;
+		unsigned b11 : 1;
+		unsigned b12 : 1;
+		unsigned b13: 1;
+		unsigned b14 : 1;
+		unsigned b15 : 1;
+		#else
+		unsigned b8 : 1;
+		unsigned b9 : 1;
+		unsigned b10 : 1;
+		unsigned b11 : 1;
+		unsigned b12 : 1;
+		unsigned b13: 1;
+		unsigned b14 : 1;
+		unsigned b15 : 1;
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+		#endif
+	} b;
 	int16_t v;
 } word_s;
 
@@ -30,6 +93,43 @@ typedef union
 			uint8_t u;
 		#endif
 	} f;
+	struct {
+		#ifdef SCP_BIG_ENDIAN
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+		unsigned b8 : 1;
+		unsigned b9 : 1;
+		unsigned b10 : 1;
+		unsigned b11 : 1;
+		unsigned b12 : 1;
+		unsigned b13: 1;
+		unsigned b14 : 1;
+		unsigned b15 : 1;
+		#else
+		unsigned b8 : 1;
+		unsigned b9 : 1;
+		unsigned b10 : 1;
+		unsigned b11 : 1;
+		unsigned b12 : 1;
+		unsigned b13: 1;
+		unsigned b14 : 1;
+		unsigned b15 : 1;
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+		#endif
+	} b;
 	uint16_t v;
 } word_u;
 
@@ -45,6 +145,88 @@ typedef union
 			int16_t u;
 		#endif
 	} f;
+	struct {
+		#ifdef SCP_BIG_ENDIAN
+			signed c0: 8;
+			unsigned c1: 8;
+			unsigned c2: 8;
+			unsigned c3: 8;
+		#else
+			unsigned c3: 8;
+			unsigned c2: 8;
+			unsigned c1: 8;
+			signed c0: 8;
+		#endif
+	} c;
+	struct {
+		#ifdef SCP_BIG_ENDIAN
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+		unsigned b8 : 1;
+		unsigned b9 : 1;
+		unsigned b10 : 1;
+		unsigned b11 : 1;
+		unsigned b12 : 1;
+		unsigned b13: 1;
+		unsigned b14 : 1;
+		unsigned b15 : 1;
+		unsigned b16 : 1;
+		unsigned b17 : 1;
+		unsigned b18 : 1;
+		unsigned b19 : 1;
+		unsigned b20 : 1;
+		unsigned b21 : 1;
+		unsigned b22 : 1;
+		unsigned b23 : 1;
+		unsigned b24 : 1;
+		unsigned b25 : 1;
+		unsigned b26 : 1;
+		unsigned b27 : 1;
+		unsigned b28 : 1;
+		unsigned b29 : 1;
+		unsigned b30 : 1;
+		unsigned b31 : 1;
+		#else
+		unsigned b24 : 1;
+		unsigned b25 : 1;
+		unsigned b26 : 1;
+		unsigned b27 : 1;
+		unsigned b28 : 1;
+		unsigned b29 : 1;
+		unsigned b30 : 1;
+		unsigned b31 : 1;
+		unsigned b16 : 1;
+		unsigned b17 : 1;
+		unsigned b18 : 1;
+		unsigned b19 : 1;
+		unsigned b20 : 1;
+		unsigned b21 : 1;
+		unsigned b22 : 1;
+		unsigned b23 : 1;
+		unsigned b8 : 1;
+		unsigned b9 : 1;
+		unsigned b10 : 1;
+		unsigned b11 : 1;
+		unsigned b12 : 1;
+		unsigned b13: 1;
+		unsigned b14 : 1;
+		unsigned b15 : 1;
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+		#endif
+	} b;
 	int32_t v;
 } dword_s;
 
@@ -60,5 +242,87 @@ typedef union
 			uint16_t u;
 		#endif
 	} f;
+	struct {
+		#ifdef SCP_BIG_ENDIAN
+			unsigned c0: 8;
+			unsigned c1: 8;
+			unsigned c2: 8;
+			unsigned c3: 8;
+		#else
+			unsigned c3: 8;
+			unsigned c2: 8;
+			unsigned c1: 8;
+			unsigned c0: 8;
+		#endif
+	} c;
+	struct {
+		#ifdef SCP_BIG_ENDIAN
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+		unsigned b8 : 1;
+		unsigned b9 : 1;
+		unsigned b10 : 1;
+		unsigned b11 : 1;
+		unsigned b12 : 1;
+		unsigned b13: 1;
+		unsigned b14 : 1;
+		unsigned b15 : 1;
+		unsigned b16 : 1;
+		unsigned b17 : 1;
+		unsigned b18 : 1;
+		unsigned b19 : 1;
+		unsigned b20 : 1;
+		unsigned b21 : 1;
+		unsigned b22 : 1;
+		unsigned b23 : 1;
+		unsigned b24 : 1;
+		unsigned b25 : 1;
+		unsigned b26 : 1;
+		unsigned b27 : 1;
+		unsigned b28 : 1;
+		unsigned b29 : 1;
+		unsigned b30 : 1;
+		unsigned b31 : 1;
+		#else
+		unsigned b24 : 1;
+		unsigned b25 : 1;
+		unsigned b26 : 1;
+		unsigned b27 : 1;
+		unsigned b28 : 1;
+		unsigned b29 : 1;
+		unsigned b30 : 1;
+		unsigned b31 : 1;
+		unsigned b16 : 1;
+		unsigned b17 : 1;
+		unsigned b18 : 1;
+		unsigned b19 : 1;
+		unsigned b20 : 1;
+		unsigned b21 : 1;
+		unsigned b22 : 1;
+		unsigned b23 : 1;
+		unsigned b8 : 1;
+		unsigned b9 : 1;
+		unsigned b10 : 1;
+		unsigned b11 : 1;
+		unsigned b12 : 1;
+		unsigned b13: 1;
+		unsigned b14 : 1;
+		unsigned b15 : 1;
+		unsigned b0 : 1;
+		unsigned b1 : 1;
+		unsigned b2 : 1;
+		unsigned b3 : 1;
+		unsigned b4 : 1;
+		unsigned b5 : 1;
+		unsigned b6 : 1;
+		unsigned b7 : 1;
+		#endif
+	} b;
 	uint32_t v;
 } dword_u;
